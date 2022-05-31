710b305e4955cff2feb07d53f1edcead3d9dedb2
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-05-31 16:43:44 +0100

    Creating ui

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import './main.css';
+
+const todos = [
+  {
+    description: 'Creating a frontend project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Baking cakes',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Reading about local storage',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Cleaning the house',
+    completed: false,
+    index: 4,
+  },
+];
+
+todos.forEach((todo) => {
+  const divEl = document.querySelector('.task-ul');
+  const div = document.createElement('div');
+  div.classList.add('todo-div');
+  div.innerHTML = `
+    <div>
+      <input type="checkbox" id="${todo.index}" />
+      </div>
+      <div>
+      <p contenteditable="true">${todo.description}</p>
+      </div>
+      <div>
+      <ion-icon name="ellipsis-vertical-outline"></ion-icon>
+      </div>
+      `;
+  divEl.appendChild(div);
+});
